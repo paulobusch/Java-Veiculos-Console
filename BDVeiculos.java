26e6d8bd7c238af15bb8b90c7e1d6145115b03a5
Paulo Ricardo Busch <paulo.ricardo@forlogic.net>
2019-06-20 16:52:13 -0300

    Consultas

--- BDVeiculos.java
+++ BDVeiculos.java
@@ -15,4 +15,20 @@ public class BDVeiculos {
     public void Insert(Carga carga){
         lstCarga.add(carga);
     }
+    public Passeio Query(Passeio passeio){
+        for(int i = 0; i < lstPasseio.size(); i++){
+            if(passeio.getPlaca() == lstPasseio.get(i).getPlaca())
+                return lstPasseio.get(i);
+        }
+
+        return null;
+    }
+    public Carga Query(Carga carga){        
+        for(int i = 0; i < lstCarga.size(); i++){
+            if(carga.getPlaca() == lstCarga.get(i).getPlaca())
+                return lstCarga.get(i);
+        }
+
+        return null;
+    }
 }
