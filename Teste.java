d976691c28922dcaa8a5be81fd0c3c614c942c79
Paulo Ricardo Busch <paulo.ricardo@forlogic.net>
2019-06-20 13:49:39 -0300

    Separação em métodos

--- Teste.java
+++ Teste.java
@@ -8,23 +8,7 @@ public class Teste{
         int opc = 0;
         do{
             do{
-                System.out.printf("Sistema de Gestão de Veículos - Menu Inicial\n\n");
-                System.out.println("1)_ Cadastrar Veículo de Passeio");
-                System.out.println("2)_ Cadastrar Veículo de Carga");
-                System.out.println("3)_ Imprimir Todos os Veículos de Passeio");
-                System.out.println("4)_ Imprimir Todos os Veículos de Carga");
-                System.out.println("5)_ Imprimir Veículo de Passeio pela Placa");
-                System.out.println("6)_ Imprimir Veículo de Carga pela Placa");
-                System.out.println("7)_ Alterar dados do Veículo de Passeio pela Placa");
-                System.out.println("8)_ Alterar dados do Veículo de Carga pela Placa");
-                System.out.println("9)_ Sair do Sistema");
-
-                try{
-                    opc = Integer.parseInt(l.entraDados("Opção: "));
-                }catch(NumberFormatException ex){                
-                    Runtime.getRuntime().exec("cls");
-                    System.out.println("Opção inválida!");
-                }
+               opc = opcaoMenu();
 
                 if(opc < 1 || opc > 9){
                     Runtime.getRuntime().exec("cls");
@@ -33,29 +17,53 @@ public class Teste{
 
             }while(opc < 1 || opc > 9);
 
-            switch(opc){
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    break;
-                case 8:
-                    break;
-                case 9:
-                    Runtime.getRuntime().exec("cls");
-                    System.out.println("Saída do sistema!");
-                    break;
-            }
+            Runtime.getRuntime().exec("cls");
+            funcaoMenu(opc);
 
         }while(opc != 9);
+    }
+    private static int opcaoMenu(){
+        System.out.printf("Sistema de Gestão de Veículos - Menu Inicial\n\n");
+        System.out.println("1)_ Cadastrar Veículo de Passeio");
+        System.out.println("2)_ Cadastrar Veículo de Carga");
+        System.out.println("3)_ Imprimir Todos os Veículos de Passeio");
+        System.out.println("4)_ Imprimir Todos os Veículos de Carga");
+        System.out.println("5)_ Imprimir Veículo de Passeio pela Placa");
+        System.out.println("6)_ Imprimir Veículo de Carga pela Placa");
+        System.out.println("7)_ Alterar dados do Veículo de Passeio pela Placa");
+        System.out.println("8)_ Alterar dados do Veículo de Carga pela Placa");
+        System.out.println("9)_ Sair do Sistema");
+
+        try{
+            return Integer.parseInt(l.entraDados("Opção: "));
+        }catch(NumberFormatException ex){                
+            Runtime.getRuntime().exec("cls");
+            System.out.println("Opção inválida!");
+        }
+
+        return 0;
+    }
+    private static void funcaoMenu(int opc){
+        switch(opc){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                System.out.println("Saída do sistema!");
+                break;
+        }
     }
 }
